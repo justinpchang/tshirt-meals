8377346d3721ccdc3b1964342f60b20fb1a467d8
justinpchang <justin.p.chang@gmail.com>
2024-02-18 09:46:54 -0500

    Initial Commit

---
+++ T-Shirt Meals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T-Shirt Meals
+//
+//  Created by Justin Chang on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T-Shirt Meals/T_Shirt_MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_Shirt_MealsApp.swift
+//  T-Shirt Meals
+//
+//  Created by Justin Chang on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct T_Shirt_MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
